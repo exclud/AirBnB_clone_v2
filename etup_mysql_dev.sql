c69a1a5234d7f2b2f4d253d3eb883e6171904ec8
exclud <shammahnjomo12@gmail.com>
2023-09-20 09:39:31 +0300

    Grant Permissions for user hbnb_test

--- etup_mysql_dev.sql
+++ etup_mysql_dev.sql
@@ -4,7 +4,7 @@
  -- Create the user if it does not exist and set the password
  CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
  -- Grant all privileges on the database to the user
- GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+ GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
  -- Grant select privileges on the performance schema to the user
  GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
  -- Apply the privilege changes immediately
