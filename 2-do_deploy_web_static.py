e1686fec40c810e542e1edcc8e8cf23ac004c769
exclud <shammahnjomo12@gmail.com>
2023-10-06 00:09:59 +0300

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to my web servers, using the function do_deploy:

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Distributes an archive to web servers."""
+from fabric.api import run, put, env, local
+import os
+
+
+env.hosts = ['277739-web-01', '277739-web-02']
+# Optional: Define these if not using default SSH keys and usernames.
+# env.user = "your_username"
+# env.key_filename = "/path/to/your/ssh/key"
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to web servers."""
+
+    if not os.path.exists(archive_path):
+        return False
+
+    # Extracting the filename without the full path
+    file_name = os.path.basename(archive_path)
+    name_without_ext = file_name.split('.')[0]
+
+    remote_path = "/tmp/{}".format(file_name)
+    data_path = "/data/web_static/releases/{}/".format(name_without_ext)
+
+    try:
+        # Upload the archive to /tmp/ directory on the web server
+        put(archive_path, remote_path)
+
+        # Uncompress the archive on the web server
+        run("mkdir -p {}".format(data_path))
+        run("tar -xzf {} -C {}".format(remote_path, data_path))
+
+        # Delete the archive from the web server
+        run("rm {}".format(remote_path))
+
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link /data/web_static/current on the web server
+        run("ln -s {} /data/web_static/current".format(data_path))
+
+        return True
+    except:
+        return False
