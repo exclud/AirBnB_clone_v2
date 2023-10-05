a424f3710670180c000069f200b9d8961d308f53
exclud <shammahnjomo12@gmail.com>
2023-10-06 00:58:33 +0300

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to my web servers, using the function do_deploy:

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
-"""Distributes an archive to web servers."""
-from fabric.api import run, put, env, local
+"""Script to generate a .tgz archive and deploy it to servers"""
+from fabric.api import local, run, put, env
+import datetime
 import os
 
+env.hosts = ['54.157.176.235', '54.144.223.110']  # updated server IPs
 
-env.hosts = ['54.157.176.235', '54.144.223.110']
 # Optional: Define these if not using default SSH keys and usernames.
-# env.user = "shammah"
-# env.key_filename = "/ssh/id_rsa/"
+# env.user = "your_username"
+# env.key_filename = "/path/to/your/ssh/key"
+
+
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder."""
+
+    # Ensure the versions directory exists
+    if not os.path.exists('versions'):
+        local('mkdir -p versions')
+
+    # Create a filename based on current datetime
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_path = 'versions/web_static_{}.tgz'.format(timestamp)
+
+    # Use the tar command to generate the .tgz
+    archive_command = 'tar -czvf {} web_static'.format(archive_path)
+    if local(archive_command).succeeded:
+        return archive_path
+    else:
+        return None
 
 
 def do_deploy(archive_path):
