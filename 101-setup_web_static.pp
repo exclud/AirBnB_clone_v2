54df0dbf1faadfcf71544242cb0b93615b5ddf85
exclud <shammahnjomo12@gmail.com>
2023-10-06 00:49:11 +0300

    Update to the puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,58 +1,42 @@
-# Ensure the apt module is present in your Puppet setup
-class { 'apt':
-  update => {
-    frequency => 'daily',
-  },
-}
+# deploy static
+$whisper_dirs = [ '/data/', '/data/web_static/',
+                        '/data/web_static/releases/', '/data/web_static/shared/',
+                        '/data/web_static/releases/test/'
+                  ]
 
-# Ensure the nginx package is installed
-package { 'nginx':
+package {'nginx':
   ensure  => installed,
-  require => Class['apt::update'],
 }
 
-# Create the necessary directories
-file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/releases/test', '/data/web_static/shared']:
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0755',
+file { $whisper_dirs:
+        ensure  => 'directory',
+        owner   => 'ubuntu',
+        group   => 'ubuntu',
+        recurse => 'remote',
+        mode    => '0777',
 }
-
-# Create the test index.html file
-file { '/data/web_static/releases/test/index.html':
-  ensure  => present,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0644',
-  content => "<html>
-<head></head>
-<body>
-    This is a test
-</body>
-</html>\n",
-}
-
-# Ensure symbolic link
 file { '/data/web_static/current':
   ensure => link,
-  target => '/data/web_static/releases/test',
-  force  => true, # This ensures that if the link exists it gets overwritten
+  target => '/data/web_static/releases/test/',
+}
+file {'/data/web_static/releases/test/index.html':
+  ensure  => present,
+  content => 'Holberton School for the win!',
 }
 
-# Update Nginx to serve content from the created directory
-file_line { 'update_nginx_for_hbnb_static':
-  path => '/etc/nginx/sites-available/default',
-  line => "\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}",
-  match => '^\tlocation / {',
-  after => '^\tlocation / {',
-  require => Package['nginx'],
-  notify => Service['nginx'],
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-# Ensure the nginx service is running
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => Package['nginx'],
+file_line {'deploy static':
+  path  => '/etc/nginx/sites-available/default',
+  after => 'server_name _;',
+  line  => "\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}",
+}
+
+service {'nginx':
+  ensure  => running,
+}
+
+exec {'/etc/init.d/nginx restart':
 }
