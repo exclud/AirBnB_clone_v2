03e28379ad732475eb4d21ea35e078f59b58e906
exclud <shammahnjomo12@gmail.com>
2023-10-22 20:25:02 +0300

    Improved the engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """DBStorage Class"""
+import models
+from models.base_model import BaseModel, Base
+from models import city, state
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+
 class DBStorage:
+    __engine = None
+    __session = None
+
+    """DBStorage class"""
     def close(self):
         """
         Call remove() method on the private session attribute or close() on the Session.
         """
-        self.__session.remove()
+        self.__session.__class__.close(self.__session)
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+import models
 from models.city import City
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
-    """ State class """
-    name = ""
+    """ class to represent states of cities"""
+    if (storage_engine == 'db'):
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
 
-    @property
-    def cities(self):
-        """
-        Getter method to return the list of City objects linked to the current State.
-        """
-        from models import storage
-        all_cities = storage.all(City)
-        return [city for city in all_cities.values() if city.state_id == self.id]
+        @property
+        def cities(self):
+            """cities list
+            """
+            result = []
+            for j, i in models.storage.all(models.city.City).items():
+                if (i.state_id == self.id):
+                    result.append(i)
+            return result
