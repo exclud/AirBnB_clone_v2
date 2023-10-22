34d4e29081523031f037e164422626fd40a2d8c0
exclud <shammahnjomo12@gmail.com>
2023-10-22 20:31:25 +0300

    Revert

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,42 +1,8 @@
 #!/usr/bin/python3
-# """
-# This module serves as the main entry point for the Flask web application.
-# """
+"""
+This module serves as the main entry point for the Flask web application.
+"""
 
-# from flask import Flask, render_template
-# from models import storage
-# from models.state import State
-
-# app = Flask(__name__)
-
-
-# @app.route('/states_list', strict_slashes=False)
-# def states_list():
-#     """
-#     Display an HTML page with a list of all
-#     State objects present in the storage.
-#     """
-#     # states = storage.all('State').values()
-#     # sorted_states = sorted(states, key=lambda x: x.name)
-#     # return render_template('7-states_list.html', states=sorted_states)
-#     data = storage.all(State)
-#     return render_template('7-states_list.html', total=data.values())
-
-
-# @app.teardown_appcontext
-# def teardown_db(exception):
-#     """
-#     Call storage.close() after each request.
-#     """
-#     if storage is not None:
-#         storage.close()
-
-
-# if __name__ == '__main__':
-#     app.run()
-
-
-"""Flask framework"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -44,19 +10,27 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """ teardown db"""
-    if storage is not None:
-        storage.close()
-
-
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ list of state ids"""
+    """
+    Display an HTML page with a list of all
+    State objects present in the storage.
+    """
+    # states = storage.all('State').values()
+    # sorted_states = sorted(states, key=lambda x: x.name)
+    # return render_template('7-states_list.html', states=sorted_states)
     data = storage.all(State)
     return render_template('7-states_list.html', total=data.values())
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    Call storage.close() after each request.
+    """
+    if storage is not None:
+        storage.close()
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
