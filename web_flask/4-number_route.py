9b1610c045bf58fcd2c836e05a5e32767004e36c
exclud <shammahnjomo12@gmail.com>
2023-10-22 19:40:49 +0300

    Flask Application Script

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+This module serves as the main entry point for the Flask web application.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Renders the greeting message "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Renders the message "HBNB".
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    Renders "C " followed by the value of the
+    text variable, replacing underscores with spaces.
+    """
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    """
+    Renders "Python " followed by the value
+    of the text variable, replacing underscores with spaces.
+    """
+    return "Python " + text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    Renders "n is a number" if n is an integer.
+    """
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run()
