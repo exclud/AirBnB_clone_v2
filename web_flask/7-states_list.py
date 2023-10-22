ad6fbfdd3777b380b02196231c634ffe690f7434
exclud <shammahnjomo12@gmail.com>
2023-10-22 20:29:01 +0300

    Added the state lisst file function

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,6 +5,7 @@ This module serves as the main entry point for the Flask web application.
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
@@ -15,9 +16,11 @@ def states_list():
     Display an HTML page with a list of all
     State objects present in the storage.
     """
-    states = storage.all('State').values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('states_list.html', states=sorted_states)
+    # states = storage.all('State').values()
+    # sorted_states = sorted(states, key=lambda x: x.name)
+    # return render_template('7-states_list.html', states=sorted_states)
+    data = storage.all(State)
+    return render_template('7-states_list.html', total=data.values())
 
 
 @app.teardown_appcontext
@@ -25,7 +28,8 @@ def teardown_db(exception):
     """
     Call storage.close() after each request.
     """
-    storage.close()
+    if storage is not None:
+        storage.close()
 
 
 if __name__ == '__main__':
